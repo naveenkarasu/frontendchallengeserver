dfcab57d7ac14860925eaa36ce77a2f4f9e95f37
naveenkarasu <naveenkarasu@gmail.com>
2025-07-01 00:30:18 -0400

    Change to include cors

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const path = require('path');
 
 // Create a server
 const server = http.createServer((req, res) => {
+    // Set CORS headers for all responses
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Handle preflight OPTIONS request
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
     if (req.url.match(/\/api\/v1\/[^/]+\/actions\/blueprints\/[^/]+\/graph/)  && req.method === 'GET') {
         const filePath = path.join(__dirname, 'graph.json');
 
@@ -14,7 +26,7 @@ const server = http.createServer((req, res) => {
                 res.end(JSON.stringify({error: 'Failed to load graph.json'}));
                 return;
             }
-            res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+            res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(data);
         });
     } else {
